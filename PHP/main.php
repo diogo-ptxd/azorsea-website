5cceec7c2979ede7e5deb4bba3707fa5b0a92597
diogo-ptxd <diogo-inacio2005@hotmail.com>
2023-11-21 16:23:12 -0100

    Added Boats

---
+++ PHP/main.php
@@ -0,0 +1,59 @@
+<?php 
+$imagembarco1 = "";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/routes/Servicos/boats/boats.css">
+    <link rel="stylesheet" href="/routes/team/team.css">
+    <title>Boat</title>
+</head>
+
+<body>
+    <div class="boat-wrapper">
+        <div class="barco-wrap">
+            <p>Espadim</p>
+            <div class="barco1">
+                <div class="imagembarco1"><img src="$imagembarco1" alt=""></div>
+                <p class="barco1-texto">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero provident
+                    debitis
+                    vel eveniet, reiciendis ipsum repellat. Vero temporibus corporis quibusdam dolorum non, impedit
+                    laboriosam ea accusantium sequi aspernatur alias suscipit.</p>
+                <div class="info-barcos">
+                    <p>Tamanho: 8m</p>
+                    <p>Capacidade: 8 Pessoas</p>
+                </div>
+
+            </div>
+
+            <style>
+            .imagembarco1{
+                width:300px;
+                height: 300px;
+            }
+                
+                </style>
+
+        </div>
+        <div class="barco-wrap">
+            <p>Gypsea Blues</p>
+            <div class="barco1">
+                <div class="imagembarco1"><img src="/images/barconovo.jpg" alt=""></div>
+                <p class="barco1-texto">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero provident
+                    debitis
+                    vel eveniet, reiciendis ipsum repellat. Vero temporibus corporis quibusdam dolorum non, impedit
+                    laboriosam ea accusantium sequi aspernatur alias suscipit.</p>
+                    <div class="info-barcos">
+                        <p>Tamanho: 10m</p>
+                        <p>Capacidade: 12 Pessoas</p>
+                    </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
